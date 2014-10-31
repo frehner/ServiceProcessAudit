eb0bc13efa6b2c0169c671e209df5f972705a96a
frehner <frehnar@hotmail.com>
2014-10-31 12:41:47 -0700

    Now the array is being output to a textarea with JSON's stringify method.

--- index.js
+++ index.js
@@ -5,12 +5,17 @@
     $scope.allSteps = [];
     $scope.step = {};
 
+    //adds a step then cleans the form
     $scope.addStep = function(){
       $scope.allSteps.push($scope.step);
       $scope.step = {};
       $scope.addStepForm.$setPristine();
-      console.log($scope.allSteps);
     };
+
+    //looks for additions to the allSteps array
+    $scope.$watchCollection('allSteps', function updatedJsonView(){
+      $scope.finalJson = JSON.stringify($scope.allSteps);
+    });
 
   });
 })();
