da6e5effe227cd87c876e9c05f4e2f6b62a907f3
frehner <frehnar@hotmail.com>
2014-12-07 22:34:52 -0700

    Editing is now working. You can select to edit any item and it will populate with the data that it has, and when you save it should update that location in the array.

--- index.js
+++ index.js
@@ -13,27 +13,88 @@
     $scope.domain1 = {};
     $scope.domain2 = {};
 
-    $scope.tempProblems = angular.copy(staticProblemArray);
+    $scope.isAnEdit = false;
+    $scope.editedStepLocation = 0;
 
     //adds a step then cleans the form
     $scope.addStep = function(){
       $scope.step.problems = [];
-
       angular.forEach($scope.tempProblems, function(tempProb){
         if (tempProb.selected) {
           $scope.step.problems.push({'type':tempProb.name, 'description':tempProb.description});
         }
       });
 
-      $scope.tempProblems = angular.copy(staticProblemArray);
-      $scope.finalJSON.steps.push($scope.step);
-      $scope.step = {};
-      $scope.addStepForm.$setPristine();
+      if ($scope.isAnEdit) {
+        $scope.finalJSON.steps[$scope.editedStepLocation] = $scope.step;
+      } else {
+        $scope.finalJSON.steps.push($scope.step);
+      }
+
+      $scope.clearForm();
     };
 
     $scope.updateJSON = function(){
       $scope.finalJSON.domains = [$scope.domain1, $scope.domain2];
       $scope.finalJson = angular.toJson($scope.finalJSON);
+    };
+
+    $scope.clearForm = function(){
+      $scope.isAnEdit = false;
+      $scope.step = {};
+      $scope.addStepForm.$setPristine();
+      angular.forEach($scope.tempProblems, function(tempProb){
+        tempProb.description = "";
+        tempProb.selected = false;
+      });
+
+    };
+
+    $scope.editStep = function(editedStep) {
+      $scope.clearForm();
+      $scope.isAnEdit = true;
+
+      var lengthOfSteps = $scope.finalJSON.steps.length;
+      for (var i = 0; i < lengthOfSteps; i++) {
+        if($scope.finalJSON.steps[i] === editedStep){
+          $scope.editedStepLocation = i;
+          break;
+        }
+      }
+
+      $scope.step.title = editedStep.title;
+      $scope.step.controlRegion = editedStep.controlRegion;
+      $scope.step.type = editedStep.type;
+      $scope.step.value_specific = editedStep.value_specific;
+      $scope.step.value_generic = editedStep.value_generic;
+      $scope.step.emphasized = editedStep.emphasized;
+
+      if(editedStep.problems){
+        var problemsCount = editedStep.problems.length;
+        for(var j = 0; j < problemsCount; j++){
+          switch (editedStep.problems[j].type) {
+          case "inconvenient":
+            $scope.tempProblems.inconvenient.selected = true;
+            $scope.tempProblems.inconvenient.description = editedStep.problems[j].description;
+            break;
+          case "confusing":
+            $scope.tempProblems.confusing.selected = true;
+            $scope.tempProblems.confusing.description = editedStep.problems[j].description;
+            break;
+          case "difficult":
+            $scope.tempProblems.difficult.selected = true;
+            $scope.tempProblems.difficult.description = editedStep.problems[j].description;
+            break;
+          case "likely_to_fail":
+            $scope.tempProblems.likely_to_fail.selected = true;
+            $scope.tempProblems.likely_to_fail.description = editedStep.problems[j].description;
+            break;
+          default:
+            break;
+          }
+        }
+      }
+
     };
 
     //looks for additions to the steps array
